65b077844b8e8d6d734baffe92d955ed334d9e0f
2alheure <jojo77.juv@live.fr>
2025-03-27 10:41:35 +0100

    Dummy exmaple of homepage variables

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,8 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
+
     #[Route("/", name: "accueil")]
     function afficherLAccueil(Request $request): Response {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'titre' => 'Accueil',
+            'date' => date('d/m/Y H:i:s'),
+            'n' => rand(0, 2)
+        ]);
     }
 }
